789e4e578d22447b79e2e087ff0f318593038535
Gregor Dschung <mail@dschung.de>
2015-09-23 17:39:23 +0200

    Integration von AppVeyor / Coverity / Coveralls

---
+++ README.md
@@ -0,0 +1,20 @@
+## Build status
+[![Build status][appveyor_build_badge]][appveyor_build_link]
+[![Test status][appveyor_tests_badge]][appveyor_tests_link]
+[![Coverity Scan Build Status][coverity_badge]][coverity_link]
+[![Coverage Status][coveralls_badge]][coveralls_link]
+
+# Bankleitzahlen-Tool
+
+## Über dieses Projekt 
+
+bla bla bla
+
+[appveyor_build_badge]: https://ci.appveyor.com/api/projects/status/m3raii5wqpxas6v3?svg=true 
+[appveyor_build_link]: https://ci.appveyor.com/project/chkpnt/bankleitzahlen-tool
+[appveyor_tests_badge]: http://teststatusbadge.azurewebsites.net/api/status/chkpnt/bankleitzahlen-tool
+[appveyor_tests_link]: https://ci.appveyor.com/project/chkpnt/bankleitzahlen-tool/build/tests
+[coverity_badge]: https://scan.coverity.com/projects/6440/badge.svg
+[coverity_link]: https://scan.coverity.com/projects/6440
+[coveralls_badge]: https://coveralls.io/repos/chkpnt/Bankleitzahlen-Tool/badge.svg 
+[coveralls_link]: https://coveralls.io/github/chkpnt/Bankleitzahlen-Tool
---
+++ build.tasks.ps1
@@ -0,0 +1,100 @@
+properties {
+  if ($env:APPVEYOR) {
+    $codecoverage = "VisualStudio2013"
+  } else {
+    $codecoverage = "disabled"
+  }
+  
+  $msbuild_exe = Resolve-Path (${env:ProgramFiles(x86)} + "\MSBuild\14.0\Bin\MSBuild.exe")
+  $vstest_console_exe = Resolve-Path ($env:VS120COMNTOOLS + "..\IDE\CommonExtensions\Microsoft\TestWindow\vstest.console.exe")
+  if ($codecoverage -eq "VisualStudio2013") {
+    $code_coverage_exe = Resolve-Path ($env:VS120COMNTOOLS + "..\..\Team Tools\Dynamic Code Coverage Tools\CodeCoverage.exe")
+  }
+  
+  $test_results_dir = "TestResults\"
+  
+  $test_assembly_pattern = "test\**\*.Tests.dll"
+  
+  $sln_file = "Bankleitzahlen-Tool.sln"
+  $coverage_xml = "coverage.coveragexml"
+  
+  $test_assemblies = @()
+}
+
+task default
+
+task RestoreNuGetPackages {
+  exec {
+    nuget.exe restore $sln_file
+  }
+}
+
+task Clean {
+  if (Test-Path $test_results_dir) {
+    Remove-Item $test_results_dir -r
+  }
+  
+  if (Test-Path $coverage_xml) {
+    Remove-Item $coverage_xml
+  }
+  
+  exec { & $msbuild_exe "/t:Clean" $sln_file }
+}
+
+task Build -depends Clean, RestoreNuGetPackages {
+  if (Test-Path Env:\APPVEYOR_BUILD_VERSION) {
+    exec { & $msbuild_exe $sln_file /Logger:"C:\Program Files\AppVeyor\BuildAgent\Appveyor.MSBuildLogger.dll" }
+  } else {
+    exec { & $msbuild_exe $sln_file }
+  }
+}
+
+task FindTestAssemblies {
+  $script:test_assemblies = @(Get-ChildItem "test\**\*.Tests.dll" -Recurse)
+}
+
+task Test -depends FindTestAssemblies -ContinueOnError {
+  $appveyor_logger_vstest_console = ""
+  if (Test-Path Env:\APPVEYOR_BUILD_VERSION) {
+    $appveyor_logger_vstest_console = "/Logger:Appveyor"
+  }
+  
+  & $vstest_console_exe /InIsolation `
+                        /EnableCodeCoverage `
+                        /Settings:CodeCoverage.runsettings `
+                        $appveyor_logger_vstest_console `
+                        /TestAdapterPath:.\packages\NUnitTestAdapter.2.0.0\lib\ `
+                        $script:test_assemblies
+  
+  if ($codecoverage -eq "VisualStudio2013") {
+    $coverage_file = Resolve-Path -path "TestResults\*\*.coverage"
+    Write-Host ("Analyzing " + $coverage_file + " and saving as " + $coverage_xml)
+    & $code_coverage_exe analyze /output:$coverage_xml "$coverage_file"
+  }
+}
+
+task ResolveCoveralls {
+  $script:coveralls_exe = Resolve-Path "packages\coveralls.net.*\tools\csmacnz.Coveralls.exe"
+}
+
+task AppVeyor-PublishCodeCoverage -depends ResolveCoveralls {
+  Write-Host $codecoverage
+  
+  if (!(Test-Path $coverage_xml)) {
+    throw ("Code-Coverage-Datei fehlt: " + $coverage_xml)
+  }
+  
+  & $script:coveralls_exe --dynamiccodecoverage `
+                          -i coverage.coveragexml `
+                          -o coverallsTestOutput.json `
+                          --repoToken $env:coveralls_token `
+                          --useRelativePaths `
+                          --commitId $env:APPVEYOR_REPO_COMMIT `
+                          --commitBranch $env:APPVEYOR_REPO_BRANCH `
+                          --commitAuthor $env:APPVEYOR_REPO_COMMIT_AUTHOR `
+                          --commitEmail $env:APPVEYOR_REPO_COMMIT_AUTHOR_EMAIL `
+                          --commitMessage $env:APPVEYOR_REPO_COMMIT_MESSAGE `
+                          --jobId $env:APPVEYOR_JOB_ID
+}
+
+task AppVeyor-TestAndPublishCodeCoverage -depends Test, AppVeyor-PublishCodeCoverage
