ab3d930c7ff3e13c3a28e5ada604ab577224831b
Gregor Dschung <mail@dschung.de>
2015-09-23 16:20:35 +0200

    Weiterentwicklung der Modelle

--- src/Bankleitzahlen.Commons/Models.cs
+++ src/Bankleitzahlen.Commons/Models.cs
@@ -9,13 +9,35 @@ namespace Bankleitzahlen.Commons
 {
     public class Bank
     {
-        public string Name { get; set; }
-        public BLZ Bankleitzahl { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public BLZ Bankleitzahl { get; set; } = BLZ.Invalid;
         public CivicAddress Adresse { get; set; }
+
+        public override string ToString()
+        {
+            var blz = Bankleitzahl.ToString();
+            var name = string.IsNullOrEmpty(Name) ? "UNBENANNTE BANK" : Name;
+            
+            if (Adresse == null)
+            {
+                return string.Format("{0} {1}", blz, name);
+            }
+
+            var stadt = string.IsNullOrEmpty(Adresse.City) ? "ORTSNAME FEHLT" : Adresse.City;
+
+            if (string.IsNullOrEmpty(Adresse.PostalCode))
+            {
+                return string.Format("{0} {1} ({2})", blz, name, stadt);
+            }
+
+            return string.Format("{0} {1} ({2} {3})", blz, name, Adresse.PostalCode, stadt);
+        }
     }
 
     public class BLZ
     {
+        public static readonly BLZ Invalid = -1;
+
         private int _value;
 
         public BLZ(int blz)
@@ -26,6 +48,16 @@ namespace Bankleitzahlen.Commons
         public static implicit operator BLZ(int blz) => new BLZ(blz);
 
         public static explicit operator int(BLZ blz) => blz._value;
+
+        public override string ToString()
+        {
+            if (this == Invalid)
+            {
+                return "--------";
+            }
+
+            return _value.ToString();
+        }
     }
 
     public class Bankleitzahlenänderungsdatei
---
+++ test/Bankleitzahlen.Commons.Tests/ModelsTests.cs
@@ -0,0 +1,67 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Device.Location;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bankleitzahlen.Commons.Tests
+{
+    [TestFixture]
+    public class ModelsTests
+    {
+        [Test]
+        public void Models_BLZ_Conversion()
+        {
+            BLZ blz = 12345;
+            Assert.That((int)blz, Is.EqualTo(12345));
+        }
+
+        [Test]
+        public void Models_BLZ_ToString()
+        {
+            BLZ blz = 12345;
+            Assert.That(blz.ToString(), Is.EqualTo("12345"));
+
+            Assert.That(BLZ.Invalid.ToString(), Is.EqualTo("--------"));
+        }
+
+        [Test]
+        public void Models_Bank_ToString_Default()
+        {
+            var bank = new Bank();
+            Assert.That(bank.ToString(), Is.EqualTo("-------- UNBENANNTE BANK"));
+        }
+
+        [Test]
+        public void Models_Bank_ToString_OhneAdresse()
+        {
+            var bank = new Bank { Bankleitzahl = 12345678, Name = "Parkbank" };
+            Assert.That(bank.ToString(), Is.EqualTo("12345678 Parkbank"));
+        }
+
+        [Test]
+        public void Models_Bank_ToString_MitAdresse()
+        {
+            var bank = new Bank { Bankleitzahl = 12345678, Name = "Parkbank", Adresse = new CivicAddress { City = "Entenhausen" } };
+            Assert.That(bank.ToString(), Is.EqualTo("12345678 Parkbank (Entenhausen)"));
+
+            bank = new Bank
+            {
+                Bankleitzahl = 12345678,
+                Name = "Parkbank",
+                Adresse = new CivicAddress { City = "Entenhausen", PostalCode = "12345" }
+            };
+            Assert.That(bank.ToString(), Is.EqualTo("12345678 Parkbank (12345 Entenhausen)"));
+
+            bank = new Bank
+            {
+                Bankleitzahl = 12345678,
+                Name = "Parkbank",
+                Adresse = new CivicAddress { PostalCode = "12345" }
+            };
+            Assert.That(bank.ToString(), Is.EqualTo("12345678 Parkbank (12345 ORTSNAME FEHLT)"));
+        }
+    }
+}
